ac1bfc49824250510f3c64de2ff3370b8777dfc6
Chandrasekhar Dubasi <chandra.dubasi@gmail.com>
2020-07-25 22:16:28 +0530

    Adding solution

---
+++ Azure-Queue-samples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_Queue_samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
